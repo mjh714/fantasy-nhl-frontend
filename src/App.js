884ccb1c7a51af9f17742c90c9202a17e78153d8
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-10-07 16:28:53 -0400

    need to finish login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import StanleyCup from './Stanley-Cup.jpg';
 import './App.css';
+import Login from "./Components/Login.js";
+import { Switch, Route, withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    user: null
+  }
+
+  loginHandler = (user) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(user)
+    }
+    fetch("http://localhost:3000/login", options)
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem("token", data.jwt)
+      this.setState({
+        user: data.user,
+        userCourses: data.user.courses
+      }, () => this.props.history.push("/users/courses"))
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Fantasy NHL</h1>
+          <img src={StanleyCup} className="App-logo" alt="Stanley-Cup" />
+          <Route exact path="/login" component={()=> <Login loginHandler={this.loginHandler} />}/> 
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
