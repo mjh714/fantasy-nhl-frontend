91f838ac4e4365633bd111592e8d063313ad1592
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-10-15 16:50:02 -0400

    mvp need to do css

--- src/Containers/LeagueContainer.js
+++ src/Containers/LeagueContainer.js
@@ -8,12 +8,22 @@ class LeagueContainer extends React.Component {
         return this.props.leagues.map(league => <League key={league.id} league={league} />)
     }
 
+    userLeagues = () => {
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        return currentUser.leagues.map(league => <League key={league.id} league={league} />)
+    }
+
     render() { 
         return (
             <>
             <Button href="leagues/create">Create A League</Button>
             <br/>
             <div style={{"background": "#B9B9B9"}}>
+                <h4>My Leagues</h4>
+                {this.userLeagues()}
+            </div>
+            <div style={{"background": "#B9B9B9"}}>
+                <h4>All Leagues</h4>
                 {this.getLeagues()}
             </div>
             </>
